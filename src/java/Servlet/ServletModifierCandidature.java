b331d9308d96a9cbe7753a5ef4f0de91675a64b2
Kentish <Kentish@Kentish-HP>
2013-05-26 23:11:15 +0200

    modif jsp

--- src/java/Servlet/ServletModifierCandidature.java
+++ src/java/Servlet/ServletModifierCandidature.java
@@ -6,6 +6,7 @@ package Servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +22,7 @@ import org.w3c.dom.Document;
  * @author Kentish
  */
 public class ServletModifierCandidature extends HttpServlet {
+
     private final String PROMOTIONS = "http://localhost:8080/Candidature/promotions";
     private final String ETATS = "http://localhost:8080/Candidature/etats";
 
@@ -42,13 +44,13 @@ public class ServletModifierCandidature extends HttpServlet {
             /* TODO output your page here. You may use following sample code. */
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ServletModifierCandidature</title>");            
+            out.println("<title>Servlet ServletModifierCandidature</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet ServletModifierCandidature at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
-        } finally {            
+        } finally {
             out.close();
         }
     }
@@ -81,34 +83,52 @@ public class ServletModifierCandidature extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        int idCandidat = Integer.parseInt(request.getParameter("idCandidat"));
-        int idEtat = Integer.parseInt(request.getParameter("idEtat"));
-        int idPromotion = Integer.parseInt(request.getParameter("idPromotion"));
-        HttpSession session=request.getSession();
-        session.setAttribute("idCandidat", idCandidat);
-        session.setAttribute("idEtat", idEtat);
-        session.setAttribute("idPromotion", idPromotion);
-        String url1 = PROMOTIONS;
-        String url2 = ETATS;
-        ClientResource resource1 = new ClientResource(url1);
-        ClientResource resource2 = new ClientResource(url2);
-        DomRepresentation reponse1 = new DomRepresentation(resource1.get());
-        Document doc1 = reponse1.getDocument();
-        DomRepresentation reponse2 = new DomRepresentation(resource2.get());
-        Document doc2 = reponse2.getDocument();
-        request.setAttribute("dom1", doc1);
-        request.setAttribute("dom2", doc2);
-        RequestDispatcher rd = request.getRequestDispatcher("modifierCandidature.jsp");
-        rd.forward(request, response);
-    }
+        int val = Integer.parseInt(request.getParameter("choix"));
+        switch (val) {
+            /*ce traitement va permettre de verifier l'identite de l'utilisateur et ses droits via les parametres 
+             * login et pass qu'il aura transmis via le formulaire useracess. On le redirige soit vers la page des 
+             * produits s'il est identifie, soit vers une page d'erreur
+             */
+            case 1:
+                int idCandidat = Integer.parseInt(request.getParameter("idCandidat"));
+                int idEtat = Integer.parseInt(request.getParameter("idEtat"));
+                int idPromotion = Integer.parseInt(request.getParameter("idPromotion"));
+                String motivation = request.getParameter("motivation");
+                String dateCandidature = request.getParameter("dateCandidature");
+                HttpSession session = request.getSession();
+                session.setAttribute("idCandidat", idCandidat);
+                session.setAttribute("idEtat", idEtat);
+                session.setAttribute("idPromotion", idPromotion);
+                session.setAttribute("motivation", motivation);
+                session.setAttribute("dateCandidature", dateCandidature);
+                String url1 = PROMOTIONS;
+                String url2 = ETATS;
+                ClientResource resource1 = new ClientResource(url1);
+                ClientResource resource2 = new ClientResource(url2);
+                DomRepresentation reponse1 = new DomRepresentation(resource1.get());
+                Document doc1 = reponse1.getDocument();
+                DomRepresentation reponse2 = new DomRepresentation(resource2.get());
+                Document doc2 = reponse2.getDocument();
+                request.setAttribute("dom1", doc1);
+                request.setAttribute("dom2", doc2);
+                RequestDispatcher rd = request.getRequestDispatcher("modifierCandidature.jsp");
+                rd.forward(request, response);
+                break;
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+            /*ce traitement va permettre de prendre en compte la demande de l'utilisateur via le radiobutton de la jsp
+             * affichecontenu. Ainsi, on initialise des attributs selon qu'il demande les produits ou les produits
+             * non vendus ou alors on le redirige vers la jsp insertionproduit s'il veut inserer un produit
+             */
+            case 2:
+                HttpSession s=request.getSession();
+                out.println(s.getAttribute("motivation"));
+                out.println(request.getParameter("etat"));
+                
+                
+                break;
+
+        }
+
+
+    }
 }
--- web/modifierCandidature.jsp
+++ web/modifierCandidature.jsp
@@ -15,21 +15,23 @@
     </head>
     <body>
 
-        <form>
+        <form action="ServletModifierCandidature" method="post">
             <select name="etat" size="1">
                 <x:forEach select="$dom2/etats/etat" var="etat">
                     <option><x:out select="$etat/@etat"/></option> 
                 </x:forEach>
             </select>
+            <input type="hidden"  name="choix"  value=2 />
+            <input name="cal" type="submit" value="Modifier" />
         </form>
         
-        <form>
+       <%-- <form>
             <select name="promo" size="1">
                 <x:forEach select="$dom1/promotions/promotion" var="promo">
                     <option><x:out select="$promo/@nom"/></option> 
                 </x:forEach>
             </select>
-        </form>
+        </form>--%>
         
     </body>
 </html>
