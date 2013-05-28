533cc011414dcdc6efabbf839ebd21c76ab8cf01
Tuonis <Tuonis@Tuonis-PC>
2013-05-28 09:47:17 +0200

    Modification d'une erreur dans ServletListePromotion Echange du get et du post.

--- src/java/Servlet/ServletListePromotion.java
+++ src/java/Servlet/ServletListePromotion.java
@@ -66,7 +66,22 @@ public class ServletListePromotion extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                response.setContentType("text/html;charset=UTF-8");
+              
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         String url3 = CANDIDATURE;
         ClientResource resource3 = null;
@@ -87,21 +102,6 @@ public class ServletListePromotion extends HttpServlet {
         }
         RequestDispatcher rd4 = request.getRequestDispatcher("index.jsp?ref=saisieCandidature");
         rd4.forward(request, response);
-    }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
     }
 
     /**
